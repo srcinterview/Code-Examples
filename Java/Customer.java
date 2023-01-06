b6c19399e19794f000cbb11e98236514c9ca4c4a
Matthew Hunt <mdhunt@scires.com>
2023-01-06 15:41:43 -0500

    Two Java files

---
+++ Java/Customer.java
@@ -0,0 +1,32 @@
+public class Customer {
+
+    private static String name;
+    private static float balance;
+
+    public Customer(String name, float balance) {
+        name = name;
+        balance = balance;
+    }
+
+    public void setName(String name) {
+        name = name;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public float getBalance(int pin) {
+        return balance;
+    }
+
+    public void makeDeposit(float deposit) {
+        balance = balance + deposit;
+    }
+
+    public void makeWithdrawal(float amount) {
+        if (balance > amount) {
+            balance = balance - amount;
+        }
+    }
+}
